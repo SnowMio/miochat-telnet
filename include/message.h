01ababc27703c357b09a8cdb04b620adba9375fd
MinimalMio <yukati2024@outlook.com>
2024-12-18 11:16:11 +0000

    [fea...t?] none

---
+++ include/message.h
@@ -0,0 +1,8 @@
+#ifndef MESSAGES
+#define MESSAGES
+
+#include <stdio.h>
+
+extern void error_message(char message[]);
+
+#endif /* MESSAGES */
--- src/main.c
+++ src/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
-int main() {
-  printf("Complete!\n");
+#include "message.h"
+
+int main(int argc, char *argv[]) {
+  if (argc != 2) {
+    error_message("Hello!");
+  }
+  /* printf("Complete!\n"); */
   return 0;
 }
